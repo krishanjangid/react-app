295f37e8d71741a92179bc287b78b4f6f1431583
krishanjangid <krishanjangid516@gmail.com>
2023-02-03 11:40:39 +0530

    added element

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(
 <div>
 <h1>Krishan kumar jangid</h1>
+<ul>
+  <li>Football</li>
+  <li>cricket</li>
+  <li>walk</li>
+  <li>have a fun</li>
+<ul>
 </div>
 ,document.getElementById('root'));
