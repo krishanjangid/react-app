6d5c1f7ee2c71a2cd4c8ae5ca6af986c6d238dc6
krishanjangid <krishanjangid516@gmail.com>
2023-02-04 12:36:09 +0530

    react learning

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+var kstyle ={
+  color:'red'
+};
+root.render(
 <div>
-<h1>Krishan kumar jangid</h1>
-<ul>
+<h1 className='heading'>Krishan kumar jangid</h1>
+<ul style={kstyle}>
   <li>Football</li>
   <li>cricket</li>
   <li>walk</li>
   <li>have a fun</li>
-<ul>
-</div>
-,document.getElementById('root'));
+  <li>Pikka Piii</li>
+</ul>
+<img src='https://picsum.photos/200'></img>
+</div>);
